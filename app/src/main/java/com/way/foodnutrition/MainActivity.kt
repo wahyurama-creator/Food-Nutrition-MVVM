7ce152feda3d380292b05e1a6f71d4efed6a3186
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-13 19:15:09 +0700

    Setup navController & bottomNavView

--- app/src/main/java/com/way/foodnutrition/MainActivity.kt
+++ app/src/main/java/com/way/foodnutrition/MainActivity.kt
@@ -2,12 +2,20 @@ package com.way.foodnutrition
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.way.foodnutrition.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerViewMain) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 
 }
