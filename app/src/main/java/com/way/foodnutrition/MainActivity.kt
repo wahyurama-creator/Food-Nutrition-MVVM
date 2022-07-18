5a964a6079c712b91e97113545a60f269238aba8
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-18 19:10:17 +0700

    Create item recipes layout

--- app/src/main/java/com/way/foodnutrition/MainActivity.kt
+++ app/src/main/java/com/way/foodnutrition/MainActivity.kt
@@ -1,21 +1,29 @@
 package com.way.foodnutrition
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.way.foodnutrition.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerViewMain) as NavHostFragment
-        val navController = navHostFragment.navController
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerViewMain) as NavHostFragment
+        navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 }
