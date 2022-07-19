fe4e88ec91114d5853e164e168e1fd47d0797962
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-19 18:00:35 +0700

    Move package

--- app/src/main/java/com/way/foodnutrition/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/way/foodnutrition/presentation/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.way.foodnutrition
+package com.way.foodnutrition.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.way.foodnutrition.R
 import com.way.foodnutrition.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
