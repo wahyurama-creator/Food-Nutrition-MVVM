090de506d59a2da84eeb71e2e8701ebbe59b5f56
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-19 18:01:23 +0700

    Create interface api endpoint

---
+++ app/src/main/java/com/way/foodnutrition/data/network/RecipesApi.kt
@@ -0,0 +1,14 @@
+package com.way.foodnutrition.data.network
+
+import com.way.foodnutrition.data.model.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface RecipesApi {
+
+    @GET("/recipes/complexSearch")
+    fun getRecipes(
+        @QueryMap queries: Map<String, String>,
+    ): Response<FoodRecipe>
+}
