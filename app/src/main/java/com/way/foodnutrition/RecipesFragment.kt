ffdb15e270f133fbcce05dc4ae58113c7870c3f9
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-19 12:07:45 +0700

    Test shimmer layout

--- app/src/main/java/com/way/foodnutrition/RecipesFragment.kt
+++ app/src/main/java/com/way/foodnutrition/RecipesFragment.kt
@@ -1,18 +1,30 @@
 package com.way.foodnutrition
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.way.foodnutrition.databinding.FragmentRecipesBinding
 
 class RecipesFragment : Fragment() {
+
+    private lateinit var binding: FragmentRecipesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+    ): View {
+        binding = FragmentRecipesBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            shimmerLayout.showShimmer(true)
+        }
     }
 
 }
