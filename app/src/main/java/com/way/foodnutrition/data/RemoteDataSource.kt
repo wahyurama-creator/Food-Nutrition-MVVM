5f9adb5939aa6b6f4c0455a0dc1e2d8867b6c526
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-20 10:21:24 +0700

    Add getRecipes function

--- app/src/main/java/com/way/foodnutrition/data/RemoteDataSource.kt
+++ app/src/main/java/com/way/foodnutrition/data/RemoteDataSource.kt
@@ -1,4 +1,15 @@
 package com.way.foodnutrition.data
 
-class RemoteDataSource {
+import com.way.foodnutrition.data.model.FoodRecipe
+import com.way.foodnutrition.data.network.RecipesApi
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val recipesApi: RecipesApi,
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> =
+        recipesApi.getRecipes(queries)
+
 }
