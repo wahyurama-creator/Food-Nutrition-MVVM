105bfcb30b04cdaf220c40b277f94ba93ab5a49a
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-20 10:22:20 +0700

    Create NetworkResult.kt for handling data from api

---
+++ app/src/main/java/com/way/foodnutrition/utils/NetworkResult.kt
@@ -0,0 +1,10 @@
+package com.way.foodnutrition.utils
+
+sealed class NetworkResult<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T) : NetworkResult<T>(data)
+    class Loading<T>(data: T? = null) : NetworkResult<T>(data)
+    class Error<T>(message: String, data: T? = null) : NetworkResult<T>(data, message)
+}
