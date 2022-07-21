14107f309addd555a4e1d92150c2acb0921a6620
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-21 11:56:37 +0700

    Create DiffUtil

---
+++ app/src/main/java/com/way/foodnutrition/presentation/ui/adapter/RecipesDiffUtil.kt
@@ -0,0 +1,25 @@
+package com.way.foodnutrition.presentation.ui.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import com.way.foodnutrition.data.model.Result
+
+class RecipesDiffUtil(
+    private val oldList: List<Result>,
+    private val newList: List<Result>,
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+}
