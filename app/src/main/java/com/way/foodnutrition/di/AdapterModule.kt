4fbf9891aacaf08263ece96a690e265ebef229a6
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-21 11:57:31 +0700

    Add adapter module

---
+++ app/src/main/java/com/way/foodnutrition/di/AdapterModule.kt
@@ -0,0 +1,17 @@
+package com.way.foodnutrition.di
+
+import com.way.foodnutrition.presentation.ui.adapter.RecipesAdapter
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AdapterModule {
+
+    @Provides
+    @Singleton
+    fun provideAdapter(): RecipesAdapter = RecipesAdapter()
+}
