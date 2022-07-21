312eb4f3702c53e3cb626230408650a4fe1bbfa5
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-21 11:56:50 +0700

    Create recipes adapter

---
+++ app/src/main/java/com/way/foodnutrition/presentation/ui/adapter/RecipesAdapter.kt
@@ -0,0 +1,63 @@
+package com.way.foodnutrition.presentation.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.way.foodnutrition.R
+import com.way.foodnutrition.data.model.Result
+import com.way.foodnutrition.databinding.ItemRecipesBinding
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class RecipesAdapter @Inject constructor() :
+    RecyclerView.Adapter<RecipesAdapter.RecipesViewHolder>() {
+
+    private var oldRecipes = emptyList<Result>()
+
+    inner class RecipesViewHolder(private val binding: ItemRecipesBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(results: Result) {
+            binding.ivRecipes.load(results.image)
+            binding.tvTitleRecipes.text = results.title
+            binding.tvDescRecipes.text = results.summary
+            binding.tvHeart.text = results.aggregateLikes.toString()
+            binding.tvClock.text = results.readyInMinutes.toString()
+            binding.apply {
+                if (results.vegan) {
+                    tvLeaf.setTextColor(ContextCompat.getColor(root.context, R.color.green))
+                    icLeaf.setColorFilter(ContextCompat.getColor(root.context, R.color.green))
+                } else {
+                    tvLeaf.setTextColor(ContextCompat.getColor(root.context, R.color.red))
+                    icLeaf.setColorFilter(ContextCompat.getColor(root.context, R.color.red))
+                }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = oldRecipes.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipesViewHolder {
+        val binding = ItemRecipesBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return RecipesViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecipesViewHolder, position: Int) {
+        holder.bind(oldRecipes[position])
+    }
+
+    fun setData(newRecipes: List<Result>) {
+        val diffUtil = RecipesDiffUtil(oldRecipes, newRecipes)
+        val diffResults = DiffUtil.calculateDiff(diffUtil)
+        oldRecipes = newRecipes
+        diffResults.dispatchUpdatesTo(this)
+    }
+
+}
