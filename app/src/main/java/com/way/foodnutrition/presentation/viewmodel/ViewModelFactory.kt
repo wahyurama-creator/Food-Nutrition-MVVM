26380b1c76a2afefaaaaa2d55154c50665a3640a
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-22 22:13:01 +0700

    Create ViewModelFactory.kt

---
+++ app/src/main/java/com/way/foodnutrition/presentation/viewmodel/ViewModelFactory.kt
@@ -0,0 +1,26 @@
+package com.way.foodnutrition.presentation.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.way.foodnutrition.data.FoodRepository
+import javax.inject.Inject
+
+class ViewModelFactory @Inject constructor(
+    private val repository: FoodRepository,
+    val app: Application
+) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(RecipesViewModel::class.java) -> {
+                RecipesViewModel(repository, app) as T
+            }
+            else -> {
+                throw IllegalArgumentException("Unknown ViewModel Class")
+            }
+        }
+    }
+
+}
