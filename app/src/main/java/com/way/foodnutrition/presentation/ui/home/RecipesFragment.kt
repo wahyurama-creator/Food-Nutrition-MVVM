f14fee3ae01d3fa44d36fab69c13081240d8a0fa
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-22 22:14:05 +0700

    Setup getRecipes from api

--- app/src/main/java/com/way/foodnutrition/presentation/ui/home/RecipesFragment.kt
+++ app/src/main/java/com/way/foodnutrition/presentation/ui/home/RecipesFragment.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.way.foodnutrition.BuildConfig
 import com.way.foodnutrition.databinding.FragmentRecipesBinding
+import com.way.foodnutrition.presentation.ui.MainActivity
+import com.way.foodnutrition.presentation.ui.adapter.RecipesAdapter
+import com.way.foodnutrition.presentation.viewmodel.RecipesViewModel
+import com.way.foodnutrition.utils.NetworkResult
 
 class RecipesFragment : Fragment() {
 
     private lateinit var binding: FragmentRecipesBinding
+    private lateinit var recipesAdapter: RecipesAdapter
+    private lateinit var recipesViewModel: RecipesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,9 +31,76 @@ class RecipesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.apply {
-            shimmerLayout.showShimmer(true)
+        recipesAdapter = (activity as MainActivity).recipesAdapter
+        recipesViewModel = (activity as MainActivity).recipesViewModel
+
+        setupRecyclerView()
+        getRecipesApi()
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvRecipes.apply {
+            visibility = View.VISIBLE
+            adapter = recipesAdapter
+            layoutManager = LinearLayoutManager(context)
+            showShimmer(false)
         }
     }
 
+    private fun showShimmer(isShow: Boolean) {
+        with(binding.shimmerLayout) {
+            visibility = View.VISIBLE
+            if (isShow) {
+                this.showShimmer(true)
+            } else {
+                visibility = View.INVISIBLE
+                this.showShimmer(false)
+            }
+        }
+    }
+
+    private fun setQueriesPathApi(): HashMap<String, String> {
+        val queries: HashMap<String, String> = HashMap()
+        queries[NUMBER] = "50"
+        queries[APIKEY] = BuildConfig.apiKey
+        queries[TYPE] = "snack"
+        queries[DIET] = "vegan"
+        queries[ADD_RECIPE_INFO] = "true"
+        queries[FILL_INGREDIENTS] = "true"
+        return queries
+    }
+
+    private fun getRecipesApi() {
+        recipesViewModel.getRecipes(setQueriesPathApi())
+        recipesViewModel.recipesResponse.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is NetworkResult.Success -> {
+                    showShimmer(false)
+                    response.data?.let {
+                        recipesAdapter.setData(it)
+                    }
+                }
+                is NetworkResult.Error -> {
+                    showShimmer(false)
+                    Toast.makeText(
+                        requireContext(),
+                        response.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                is NetworkResult.Loading -> {
+                    showShimmer(true)
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val NUMBER = "number"
+        const val APIKEY = "apiKey"
+        const val TYPE = "type"
+        const val DIET = "diet"
+        const val ADD_RECIPE_INFO = "addRecipeInformation"
+        const val FILL_INGREDIENTS = "fillIngredients"
+    }
 }
