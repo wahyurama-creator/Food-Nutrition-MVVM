24c9687335e82fdd54ef25aa0f3b734e9f9de98c
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-22 22:15:38 +0700

    Add inject variable on activity

--- app/src/main/java/com/way/foodnutrition/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/way/foodnutrition/presentation/ui/MainActivity.kt
@@ -2,20 +2,35 @@ package com.way.foodnutrition.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.way.foodnutrition.R
 import com.way.foodnutrition.databinding.ActivityMainBinding
+import com.way.foodnutrition.presentation.ui.adapter.RecipesAdapter
+import com.way.foodnutrition.presentation.viewmodel.RecipesViewModel
+import com.way.foodnutrition.presentation.viewmodel.ViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var recipesAdapter: RecipesAdapter
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+    lateinit var recipesViewModel: RecipesViewModel
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        recipesViewModel = ViewModelProvider(this, viewModelFactory)[RecipesViewModel::class.java]
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerViewMain) as NavHostFragment
