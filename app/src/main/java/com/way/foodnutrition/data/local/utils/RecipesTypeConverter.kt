90ea7edfc140b141da0c91cfbd3811fb9886fc8e
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-26 20:39:16 +0700

    Remove dependency injection in RecipesTypeConverter.kt

--- app/src/main/java/com/way/foodnutrition/data/local/utils/RecipesTypeConverter.kt
+++ app/src/main/java/com/way/foodnutrition/data/local/utils/RecipesTypeConverter.kt
@@ -4,20 +4,20 @@ import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.way.foodnutrition.data.remote.model.FoodRecipe
-import javax.inject.Inject
 
 class RecipesTypeConverter {
 
-    @Inject
-    lateinit var gson: Gson
+    private lateinit var gson: Gson
 
     @TypeConverter
     fun foodRecipeToString(foodRecipe: FoodRecipe): String {
+        gson = Gson()
         return gson.toJson(foodRecipe)
     }
 
     @TypeConverter
     fun stringToFoodRecipe(data: String): FoodRecipe {
+        gson = Gson()
         val listOfType = object : TypeToken<FoodRecipe>() {}.type
         return gson.fromJson(data, listOfType)
     }
