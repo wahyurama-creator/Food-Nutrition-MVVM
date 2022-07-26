6adb905e2a10c21c4eb0dfae81fcf49471e9f66e
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-26 20:39:43 +0700

    Create variable for logging

---
+++ app/src/main/java/com/way/foodnutrition/utils/TrackLog.kt
@@ -0,0 +1,6 @@
+package com.way.foodnutrition.utils
+
+object TrackLog {
+    const val REQUEST_GET_API = "REQUEST GET API CALLED"
+    const val GET_DATA_FROM_DATABASE = "GET DATA FROM DATABASE CALLED"
+}
