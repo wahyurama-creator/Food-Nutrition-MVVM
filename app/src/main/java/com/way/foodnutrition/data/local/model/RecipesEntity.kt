82a70139b0a3efdf9c05d3b85b8eb16aca20d3e0
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-26 10:35:25 +0700

    Create recipes entity for offline caching

---
+++ app/src/main/java/com/way/foodnutrition/data/local/model/RecipesEntity.kt
@@ -0,0 +1,16 @@
+package com.way.foodnutrition.data.local.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.way.foodnutrition.data.local.model.RecipesEntity.Companion.TABLE_RECIPES
+import com.way.foodnutrition.data.model.FoodRecipe
+
+@Entity(tableName = TABLE_RECIPES)
+class RecipesEntity(var foodRecipe: FoodRecipe) {
+    @PrimaryKey(autoGenerate = false)
+    val id: Int = 0
+
+    companion object {
+        const val TABLE_RECIPES = "recipes_table"
+    }
+}
