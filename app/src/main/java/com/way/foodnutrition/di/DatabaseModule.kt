d5ab156291da9590e29d5410d3e7309166a92b94
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-26 10:56:20 +0700

    Create database module

---
+++ app/src/main/java/com/way/foodnutrition/di/DatabaseModule.kt
@@ -0,0 +1,35 @@
+package com.way.foodnutrition.di
+
+import android.content.Context
+import androidx.room.Room
+import com.way.foodnutrition.data.local.model.RecipesEntity.Companion.DATABASE_RECIPES
+import com.way.foodnutrition.data.local.room.RecipesDatabase
+import com.way.foodnutrition.data.local.utils.RecipesTypeConverter
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        RecipesDatabase::class.java,
+        DATABASE_RECIPES
+    ).addTypeConverter(RecipesTypeConverter::class.java)
+        .fallbackToDestructiveMigration()
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideDao(database: RecipesDatabase) = database.recipesDao()
+
+}
