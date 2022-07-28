5188f0b901236b5cd1a6e579931789de8ab399e2
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-28 18:07:03 +0700

    Create bottom sheet dialog interface for recipes

---
+++ app/src/main/java/com/way/foodnutrition/presentation/ui/home/component/RecipesBottomSheetFragment.kt
@@ -0,0 +1,20 @@
+package com.way.foodnutrition.presentation.ui.home.component
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.way.foodnutrition.R
+
+class RecipesBottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipes_bottom_sheet, container, false)
+    }
+}
