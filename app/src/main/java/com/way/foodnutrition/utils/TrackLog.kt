94696d79495700a6bb0b43b42dda422524d73c0f
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-31 14:54:56 +0700

    Add variable for chips logging

--- app/src/main/java/com/way/foodnutrition/utils/TrackLog.kt
+++ app/src/main/java/com/way/foodnutrition/utils/TrackLog.kt
@@ -3,4 +3,8 @@ package com.way.foodnutrition.utils
 object TrackLog {
     const val REQUEST_GET_API = "REQUEST GET API CALLED"
     const val GET_DATA_FROM_DATABASE = "GET DATA FROM DATABASE CALLED"
+
+    // Chip
+    const val SELECTED_MEAL = "SELECTED MEAL"
+    const val SELECTED_DIET = "SELECTED DIET"
 }
