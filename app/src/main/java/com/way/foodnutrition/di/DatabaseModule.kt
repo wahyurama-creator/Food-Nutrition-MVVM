48931913b4d0a252ac5d26c925bf9227eb8224e7
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-07-31 14:54:15 +0700

    Add data store repository

--- app/src/main/java/com/way/foodnutrition/di/DatabaseModule.kt
+++ app/src/main/java/com/way/foodnutrition/di/DatabaseModule.kt
@@ -1,7 +1,12 @@
 package com.way.foodnutrition.di
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 import androidx.room.Room
+import com.way.foodnutrition.data.DataStoreRepository.Companion.PREFERENCES_KEY
 import com.way.foodnutrition.data.local.model.RecipesEntity.Companion.DATABASE_RECIPES
 import com.way.foodnutrition.data.local.room.RecipesDatabase
 import dagger.Module
@@ -23,11 +28,18 @@ object DatabaseModule {
         context,
         RecipesDatabase::class.java,
         DATABASE_RECIPES
-    )
-        .build()
+    ).build()
 
     @Provides
     @Singleton
     fun provideDao(database: RecipesDatabase) = database.recipesDao()
 
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> =
+        PreferenceDataStoreFactory.create(
+            produceFile = {
+                context.preferencesDataStoreFile(PREFERENCES_KEY)
+            }
+        )
 }
