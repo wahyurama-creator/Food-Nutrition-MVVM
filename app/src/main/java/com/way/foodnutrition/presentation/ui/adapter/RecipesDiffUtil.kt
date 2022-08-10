abe691328f99a02f103c1313894285b5cc35f8a2
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-08-10 12:03:25 +0700

    Create generics

--- app/src/main/java/com/way/foodnutrition/presentation/ui/adapter/RecipesDiffUtil.kt
+++ app/src/main/java/com/way/foodnutrition/presentation/ui/adapter/RecipesDiffUtil.kt
@@ -3,9 +3,9 @@ package com.way.foodnutrition.presentation.ui.adapter
 import androidx.recyclerview.widget.DiffUtil
 import com.way.foodnutrition.data.remote.model.Result
 
-class RecipesDiffUtil(
-    private val oldList: List<Result>,
-    private val newList: List<Result>,
+class RecipesDiffUtil<T>(
+    private val oldList: List<T>,
+    private val newList: List<T>,
 ) : DiffUtil.Callback() {
     override fun getOldListSize(): Int {
         return oldList.size
