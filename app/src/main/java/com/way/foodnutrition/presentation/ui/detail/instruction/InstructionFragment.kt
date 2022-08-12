b9628ab9cd1af1c1b9473bc9e2e700cc398e71da
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-08-12 14:19:20 +0700

    Setup webView

--- app/src/main/java/com/way/foodnutrition/presentation/ui/detail/instruction/InstructionFragment.kt
+++ app/src/main/java/com/way/foodnutrition/presentation/ui/detail/instruction/InstructionFragment.kt
@@ -5,14 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.way.foodnutrition.R
+import com.way.foodnutrition.data.remote.model.Result
+import com.way.foodnutrition.databinding.FragmentInstructionBinding
+import com.way.foodnutrition.presentation.ui.detail.DetailActivity
 
 class InstructionFragment : Fragment() {
+
+    private lateinit var binding: FragmentInstructionBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_instruction, container, false)
+        binding = FragmentInstructionBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val argsBundle = arguments?.getParcelable<Result>(DetailActivity.RECIPES_BUNDLE) as Result
+        setupWebView(argsBundle.sourceUrl)
+    }
+
+    private fun setupWebView(url: String) {
+        binding.webViewInstruction.apply {
+            webViewClient = object : WebViewClient() {}
+            loadUrl(url)
+            settings.javaScriptEnabled = true
+        }
     }
 }
