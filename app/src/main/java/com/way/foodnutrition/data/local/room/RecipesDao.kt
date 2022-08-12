1465137ce3c8951ab0f95cf4d0e1e304c21f70d8
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-08-12 17:01:43 +0700

    Add CRUD for favorite entity

--- app/src/main/java/com/way/foodnutrition/data/local/room/RecipesDao.kt
+++ app/src/main/java/com/way/foodnutrition/data/local/room/RecipesDao.kt
@@ -1,10 +1,9 @@
 package com.way.foodnutrition.data.local.room
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+import com.way.foodnutrition.data.local.model.FavoriteEntity
 import com.way.foodnutrition.data.local.model.RecipesEntity
+import com.way.foodnutrition.data.remote.model.Result
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -14,4 +13,16 @@ interface RecipesDao {
 
     @Query("SELECT * FROM recipes_table ORDER BY id ASC")
     fun readRecipes(): Flow<List<RecipesEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoriteRecipes(result: Result)
+
+    @Query("SELECT * FROM favorite_recipes_table ORDER BY id ASC")
+    fun readFavoriteRecipes(): Flow<List<FavoriteEntity>>
+
+    @Delete
+    suspend fun deleteFavoriteRecipes(favoriteEntity: FavoriteEntity)
+
+    @Query("DELETE FROM favorite_recipes_table")
+    suspend fun deleteAllFavoriteRecipes()
 }
