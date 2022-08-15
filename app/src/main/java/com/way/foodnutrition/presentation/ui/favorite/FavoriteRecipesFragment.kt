b24279dc1c16c7258b4583b594dd14a31b950802
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-08-15 22:24:50 +0700

    Add menu for delete all favorite

--- app/src/main/java/com/way/foodnutrition/presentation/ui/favorite/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/way/foodnutrition/presentation/ui/favorite/FavoriteRecipesFragment.kt
@@ -1,14 +1,17 @@
 package com.way.foodnutrition.presentation.ui.favorite
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
+import android.view.*
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
-import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.way.foodnutrition.R
 import com.way.foodnutrition.databinding.FragmentFavoriteRecipesBinding
 import com.way.foodnutrition.presentation.ui.MainActivity
 import com.way.foodnutrition.presentation.ui.adapter.FavoriteAdapter
@@ -42,6 +45,7 @@ class FavoriteRecipesFragment : Fragment() {
         detailViewModel = ViewModelProvider(this, factory)[DetailViewModel::class.java]
 
         setupRecyclerView()
+        setOptionMenu()
 
         detailViewModel.readFavoriteRecipes.observe(viewLifecycleOwner) { favoriteEntity ->
             if (favoriteEntity.isNullOrEmpty()) {
@@ -71,5 +75,31 @@ class FavoriteRecipesFragment : Fragment() {
             lottieAnimationView.visibility = if (isShow) VISIBLE else INVISIBLE
             tvTitle.visibility = if (isShow) VISIBLE else INVISIBLE
         }
+    }
+
+    private fun setOptionMenu() {
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.favorite_recipes_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.deleteAllRecipesMenu -> {
+                        detailViewModel.deleteAllFavoriteRecipes()
+                        Snackbar.make(
+                            binding.root,
+                            getString(R.string.all_recipes_removed),
+                            Snackbar.LENGTH_SHORT
+                        )
+                            .setAction("Okay") {}
+                            .show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 }
