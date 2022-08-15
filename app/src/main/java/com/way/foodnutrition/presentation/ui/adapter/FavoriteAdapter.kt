f126795c391e6884fe961e71c054c3aef2238123
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-08-15 19:32:36 +0700

    Add multiple selection

--- app/src/main/java/com/way/foodnutrition/presentation/ui/adapter/FavoriteAdapter.kt
+++ app/src/main/java/com/way/foodnutrition/presentation/ui/adapter/FavoriteAdapter.kt
@@ -19,15 +19,16 @@ class FavoriteAdapter(
 ) : RecyclerView.Adapter<FavoriteAdapter.FavoriteViewHolder>(), ActionMode.Callback {
 
     private var oldFavorite = emptyList<FavoriteEntity>()
+    private var multiSelection = false
+    private var selectedRecipes = arrayListOf<FavoriteEntity>()
+    private var favBinding = arrayListOf<ItemRecipesBinding>()
 
     inner class FavoriteViewHolder(private val binding: ItemRecipesBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(favoriteEntity: FavoriteEntity) {
-            binding.ivRecipes.load(favoriteEntity.result.image) {
-                crossfade(true)
-                placeholder(R.drawable.ic_error_placeholder)
-                error(R.drawable.ic_error_placeholder)
-            }
+            favBinding.add(binding)
+
+            binding.ivRecipes.load(favoriteEntity.result.image)
             binding.tvTitleRecipes.text = favoriteEntity.result.title
             binding.tvDescRecipes.text = Jsoup.parse(favoriteEntity.result.summary).text()
             binding.tvHeart.text = favoriteEntity.result.aggregateLikes.toString()
@@ -45,15 +46,26 @@ class FavoriteAdapter(
 
             binding.root.apply {
                 setOnClickListener {
-                    val action =
-                        FavoriteRecipesFragmentDirections.actionFavoriteRecipesFragmentToDetailActivity(
-                            favoriteEntity.result
-                        )
-                    binding.root.findNavController().navigate(action)
+                    if (multiSelection) {
+                        checkSelection(binding, favoriteEntity)
+                    } else {
+                        val action =
+                            FavoriteRecipesFragmentDirections.actionFavoriteRecipesFragmentToDetailActivity(
+                                favoriteEntity.result
+                            )
+                        binding.root.findNavController().navigate(action)
+                    }
                 }
                 setOnLongClickListener {
-                    requireActivity.startActionMode(this@FavoriteAdapter)
-                    true
+                    if (!multiSelection) {
+                        multiSelection = true
+                        requireActivity.startActionMode(this@FavoriteAdapter)
+                        checkSelection(binding, favoriteEntity)
+                        true
+                    } else {
+                        checkSelection(binding, favoriteEntity)
+                        false
+                    }
                 }
             }
         }
@@ -81,6 +93,31 @@ class FavoriteAdapter(
         diffResults.dispatchUpdatesTo(this)
     }
 
+    private fun checkSelection(binding: ItemRecipesBinding, currentRecipes: FavoriteEntity) {
+        if (selectedRecipes.contains(currentRecipes)) {
+            selectedRecipes.remove(currentRecipes)
+            changeItemStyle(binding, R.color.white, R.color.lightMediumGray)
+        } else {
+            selectedRecipes.add(currentRecipes)
+            changeItemStyle(binding, R.color.purple_500, R.color.purple_700)
+        }
+    }
+
+    private fun changeItemStyle(
+        binding: ItemRecipesBinding,
+        @ColorInt backgroundColor: Int,
+        @ColorInt strokeColor: Int
+    ) {
+        binding.cardViewItemRecipes.setBackgroundColor(
+            ContextCompat.getColor(
+                requireActivity,
+                backgroundColor
+            )
+        )
+        binding.cardViewItemRecipes.strokeColor =
+            ContextCompat.getColor(requireActivity, strokeColor)
+    }
+
     override fun onCreateActionMode(actionMode: ActionMode?, menu: Menu?): Boolean {
         actionMode?.menuInflater?.inflate(R.menu.favorite_contextual_menu, menu)
         changeStatusBarColor(R.color.darker)
@@ -89,9 +126,17 @@ class FavoriteAdapter(
 
     override fun onPrepareActionMode(p0: ActionMode?, p1: Menu?): Boolean = true
     override fun onActionItemClicked(p0: ActionMode?, p1: MenuItem?): Boolean = true
-    override fun onDestroyActionMode(p0: ActionMode?) = changeStatusBarColor(R.color.purple_500)
+    override fun onDestroyActionMode(p0: ActionMode?) {
+        favBinding.forEach { binding ->
+            changeItemStyle(binding, R.color.white, R.color.lightMediumGray)
+        }
+        multiSelection = false
+        changeStatusBarColor(R.color.purple_500)
+        selectedRecipes.clear()
+    }
 
     private fun changeStatusBarColor(@ColorInt color: Int) {
         requireActivity.window.statusBarColor = ContextCompat.getColor(requireActivity, color)
     }
 }
+
